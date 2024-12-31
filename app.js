3d00b41f87926b5bb992257887fdba66ca2b0f51
Riyajul2002 <riyajulislam9382@gmail.com>
2024-12-31 19:46:02 +0530

    imageSearch

---
+++ app.js
@@ -0,0 +1,67 @@
+const accessKey = "dhJRAxAaZJBNfTm4mt3A8OzwNpei-M3fdjL9X2HN6rk";
+
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-image");
+const searchResult = document.querySelector(".search-results");
+const showMore = document.getElementById("more-btn");
+
+let inputData = "";
+let page = 1;
+
+async function searchImage(){
+    inputData = inputEl.value;
+  
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    
+    const results = data.results;
+   
+    if (page === 1){
+        searchResult.innerHTML = ""
+    }
+
+    results.map((result) =>{
+       const imageWrapper = document.createElement("div");
+       imageWrapper.classList.add("search-result");
+       const image = document.createElement("img");
+       image.src = result.urls.small;
+       image.alt = result.alt_description;
+       const imageLink = document.createElement("a");
+       imageLink.href = result.links.html;
+       imageLink.target = "_blank";
+       imageLink.textContent = result.alt_description;
+
+
+       imageWrapper.appendChild(image);
+       imageWrapper.appendChild(imageLink);
+       searchResult.appendChild(imageWrapper);
+
+       console.log(result);
+    });
+
+
+page++
+if(page > 1){
+  showMore.style.display = "block"  
+}
+}
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    page = 1;
+    searchImage()
+})
+
+showMore.addEventListener("click",() =>{
+
+    searchImage()
+})
+
+
+
+
+
